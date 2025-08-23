619ef33db43f431b0f14183d883ecb74c91b4a02
JairoManchay <jairorodrigomanchayperez@gmail.com>
2025-08-23 10:23:53 -0500

    Agregando cambios de responsive desing

---
+++ js/main.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".top", {
+    duration: 1500,
+    origin: "top",
+    distance: "70px",
+})
+
+sr.reveal(".left", {
+    duration: 2000,
+    origin: "left",
+    distance: "70px",
+})
+
+sr.reveal(".rigth", {
+    duration: 2000,
+    origin: "rigth",
+    distance: "70px",
+})
+
+sr.reveal(".bottom", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "70px",
+})
